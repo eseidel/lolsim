f49cd0ef016a753ae394aefbfe61eff7f3169a6c
Eric Seidel <github@eseidel.com>
2017-01-06 17:24:16 -0800

    Add forgotten masteries_test file

---
+++ test/masteries_test.dart
@@ -0,0 +1,100 @@
+import "package:test/test.dart";
+import "package:lol_duel/dragon.dart";
+import 'test_mob.dart';
+
+main() async {
+  DragonData data = await DragonData.loadLatest();
+
+  Mastery masteryByName(String name, int rank) {
+    MasteryDescription description = data.masteries.masteryByName(name);
+    return new Mastery(description, rank);
+  }
+
+  group("Double Edged Sword", () {
+    test("damage amp", () {
+      Mob doubleEdgeMob = createTestMob(
+          hp: 1000.0,
+          ad: 100.0,
+          masteries: [masteryByName('Double Edged Sword', 1)]);
+      Mob normalMob = createTestMob(hp: 1000.0, ad: 100.0);
+      World world = new World();
+      new AutoAttack(doubleEdgeMob, normalMob).apply(world);
+      new AutoAttack(normalMob, doubleEdgeMob).apply(world);
+
+      // Take 2.5% more damage, deal 5% more damage.
+      expect(doubleEdgeMob.currentHp, 897.5);
+      expect(normalMob.currentHp, 895.0);
+    });
+  });
+  group("Tough Skin", () {
+    test("damage reduction", () {
+      Mob toughSkinMob =
+          createTestMob(hp: 100.0, masteries: [masteryByName('Tough Skin', 1)]);
+      Mob champion = createTestMob(ad: 10.0, type: MobType.champion);
+      Mob monster = createTestMob(ad: 10.0, type: MobType.monster);
+      Mob minion = createTestMob(ad: 10.0, type: MobType.minion);
+      World world = new World();
+      new AutoAttack(champion, toughSkinMob).apply(world);
+      expect(toughSkinMob.currentHp, 92.0);
+      new AutoAttack(monster, toughSkinMob).apply(world);
+      expect(toughSkinMob.currentHp, 84.0);
+      new AutoAttack(minion, toughSkinMob).apply(world);
+      expect(toughSkinMob.currentHp, 74.0);
+    });
+  });
+  group("Merciless", () {
+    test("damage amp", () {
+      Mob mercilessMob =
+          createTestMob(hp: 100.0, masteries: [masteryByName('Merciless', 5)]);
+      Mob healthyMob = createTestMob(hp: 1000.0);
+      Hit physicalHit = mercilessMob.createHitForTarget(
+        target: healthyMob,
+        physicalDamage: 10.0,
+      );
+      expect(10.0, healthyMob.applyHit(physicalHit));
+
+      Mob hurtMob = createTestMob(hp: 1000.0);
+      hurtMob.hpLost = 601.0;
+      expect(hurtMob.healthPercent, lessThan(0.4));
+      Hit healthyHit = mercilessMob.createHitForTarget(
+        target: hurtMob,
+        physicalDamage: 10.0,
+      );
+      expect(10.5, hurtMob.applyHit(healthyHit));
+      Hit magicalHit = mercilessMob.createHitForTarget(
+        target: hurtMob,
+        physicalDamage: 10.0,
+      );
+      expect(10.5, hurtMob.applyHit(magicalHit));
+      Hit mixedHit = mercilessMob.createHitForTarget(
+        target: hurtMob,
+        physicalDamage: 10.0,
+        magicDamage: 10.0,
+      );
+      expect(21.0, hurtMob.applyHit(mixedHit));
+    });
+  });
+  group("Savagery", () {
+    test("damage amp", () {
+      Mob savageryMob =
+          createTestMob(hp: 100.0, masteries: [masteryByName('Savagery', 5)]);
+      Mob normalMob = createTestMob(hp: 100.0);
+      Hit physicalHit = savageryMob.createHitForTarget(
+        target: normalMob,
+        physicalDamage: 10.0,
+      );
+      expect(15.0, normalMob.applyHit(physicalHit));
+      Hit magicalHit = savageryMob.createHitForTarget(
+        target: normalMob,
+        physicalDamage: 10.0,
+      );
+      expect(15.0, normalMob.applyHit(magicalHit));
+      Hit mixedHit = savageryMob.createHitForTarget(
+        target: normalMob,
+        physicalDamage: 10.0,
+        magicDamage: 10.0,
+      );
+      expect(25.0, normalMob.applyHit(mixedHit));
+    });
+  });
+}
