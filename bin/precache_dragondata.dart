edc77e12d4bd1cf7b46722d9577190bf3a56fd81
Eric Seidel <github@eseidel.com>
2017-02-05 22:10:28 -0800

    Give precache_dragondata a success message

--- bin/precache_dragondata.dart
+++ bin/precache_dragondata.dart
@@ -3,4 +3,5 @@ import 'package:lol_duel/dragon_loader.dart';
 
 void main() {
   DragonData.loadLatest(loader: new NetworkLoader());
+  print("Successfully cached dragon data to $DEFAULT_CACHE_DIR");
 }
